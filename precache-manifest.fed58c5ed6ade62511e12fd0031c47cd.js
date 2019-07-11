c2390630e0a89cef717f378c4de5c3ef3072fcd4
Sam-Main <sam.joseph.donaldson@gmail.com>
2019-07-12 09:52:04 +1200

    Updates

---
+++ precache-manifest.fed58c5ed6ade62511e12fd0031c47cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94ece601c4ad4d1268d8def25d82a76",
+    "url": "/random-quote-react/index.html"
+  },
+  {
+    "revision": "003c707240f07e62e63a",
+    "url": "/random-quote-react/static/css/main.40f37c83.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/random-quote-react/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "003c707240f07e62e63a",
+    "url": "/random-quote-react/static/js/main.9d01948c.chunk.js"
+  },
+  {
+    "revision": "6fe7453b2ddf7615832b",
+    "url": "/random-quote-react/static/js/runtime~main.fa05e5a7.js"
+  }
+]);
